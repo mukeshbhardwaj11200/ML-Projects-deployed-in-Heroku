ed93841d2df6eee070442b80bfb7fc70951d68d1
Mukesh Bhardwaj <51175224+mukeshbhardwaj11200@users.noreply.github.com>
2020-06-12 16:16:00 +0530

    Add files via upload

---
+++ Iris Flower Classification/app.py
@@ -0,0 +1,28 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+import os
+
+app= Flask(__name__,template_folder=r'C:\Users\Bhardwaj\Desktop\IRIS',static_url_path='/static')
+model= pickle.load(open('model.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template("iris.html")
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    int_features = [x for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    
+    if(prediction==0):
+        output="SETOSA"
+    elif(prediction==1):
+        output='VERSICOLOR'
+    else:
+        output='VIRGINICA'
+    return render_template('iris.html',prediction_text="Predicted Flower is {}".format(str(output)))
+
+if __name__ == "__main__":
+    app.run()
